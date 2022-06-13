09e1e52a2ec2f3a44d28fe34c5234b3483d64248
Anna Lozovaya <anyalozovaya2014@gmail.com>
2022-06-13 23:49:53 +0300

    feat: add project

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-app>
+    <h1>DRAG AND DROP POJECT</h1>
+
+    <Desk
+      class="todo-app"
+      v-bind:categories="categories"
+      v-bind:tasks="tasks"
+    />
+  </v-app>
+</template>
+
+<script>
+import Desk from "../src/components/Desk/Desk";
+
+export default {
+  name: "App",
+  components: {
+    Desk,
+  },
+  data: () => {
+    return {
+      tasks: [
+        {
+          id: 2,
+          title: "Вымыть полы",
+          categoryId: 2,
+        },
+        {
+          id: 1,
+          title: "Сходить к стоматологу",
+          categoryId: 0,
+        },
+        {
+          id: 0,
+          title: "Написать отчет",
+          categoryId: 1,
+        },
+        {
+          id: 3,
+          title: "Вынести мусор",
+          categoryId: 2,
+        },
+        {
+          id: 4,
+          title: "Сходить в магазин",
+          categoryId: 0,
+        },
+      ],
+      categories: [
+        {
+          id: 0,
+          title: "Сделать",
+        },
+        {
+          id: 1,
+          title: "В процессе",
+        },
+        {
+          id: 2,
+          title: "Закончено",
+        },
+      ],
+    };
+  },
+  
+};
+</script>
+
+<style lang="scss">
+@import "./assets/scss/main.scss";
+
+h1 {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  @include title($black);
+  font-size: 25px;
+  margin: 20px 0;
+}
+</style>
---
+++ src/components/Desk/Desk.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="todo-app">
+    <div
+      v-for="category in categories"
+      :key="category.id"
+      class="desk"
+      @drop="onDrop($event, category.id)"
+      @dragover.prevent
+      @dragenter.prevent
+    >
+      <div class="desk_title">
+        <h4>{{ category.title }}</h4>
+      </div>
+      <div class="desk_tasks">
+        <Task
+          v-for="(task, index) in items.filter(
+            (x) => x.categoryId === category.id
+          )"
+          :key="task.id"
+          :style="{ top: `${index * 40}px` }"
+          @dragstart="onDragStart($event, task)"
+          draggable="true"
+          v-bind:task="task"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Task from "../../components/Task/Task";
+
+export default {
+  name: "Desk",
+  props: ["categories", "tasks"],
+  components: {
+    Task,
+  },
+  data: () => {
+    return {
+      items: [],
+      desks: [],
+    };
+  },
+  watch: {
+    tasks: {
+      handler() {
+        this.items = this.tasks;
+      },
+      deep: true,
+    },
+    items() {
+      this.$emit("update:tasks", this.items);
+    },
+  },
+  methods: {
+    onDragStart(event, task) {
+      event.dataTransfer.dropEffect = "none";
+      event.dataTransfer.effectAllowed = "move";
+      event.dataTransfer.setData("taskId", task.id.toString());
+    },
+    onDrop(event, categoryId) {
+      event.dataTransfer.dropEffect = "move";
+      const taskId = parseInt(event.dataTransfer.getData("taskId"));
+      this.items.value = this.items.map((x) => {
+        if (x.id == taskId) {
+          x.categoryId = categoryId;
+        }
+        return x;
+      });
+    },
+  },
+  beforeMount() {
+    this.items = this.tasks;
+  },
+};
+</script>
+
+<style lang="scss" >
+</style>
