cb831220024f587b53bd65f2db2a4eebc811dfdb
Anna Lozovaya <anyalozovaya2014@gmail.com>
2022-06-26 19:38:08 +0300

    fix: change css

--- src/App.vue
+++ src/App.vue
@@ -81,15 +81,4 @@ export default {
 
 <style lang="scss">
 @import "./assets/scss/main.scss";
-
-h1 {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  @include title($black);
-  font-size: 25px;
-  margin: 20px 0;
-  padding: 0 0 10px 0;
-  border-bottom: 2px solid $purple;
-}
 </style>
